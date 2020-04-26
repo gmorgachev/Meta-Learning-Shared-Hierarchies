dbf1c83c35b032111a1627dbf6b444a36f1a1118
Gleb.Morgachev <Gleb.Morgachev@jetbrains.com>
2020-04-27 00:22:24 +0300

    Init model from torch tutorial

---
+++ src/utils.py
@@ -0,0 +1,129 @@
+import random
+import torch
+import torchvision.transforms as T
+import numpy as np
+import matplotlib.pyplot as plt
+
+from collections import namedtuple
+from torch.nn import functional as F
+from IPython import display
+from PIL import Image
+
+Transition = namedtuple(
+    'Transition',
+    ('state', 'action', 'next_state', 'reward'))
+
+
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+
+resize = T.Compose([T.ToPILImage(),
+                    T.Resize(40, interpolation=Image.CUBIC),
+                    T.ToTensor()])
+
+
+def get_cart_location(env, screen_width):
+    world_width = env.x_threshold * 2
+    scale = screen_width / world_width
+    return int(env.state[0] * scale + screen_width / 2.0)  # MIDDLE OF CART
+
+
+def get_screen(env, device):
+    screen = env.render(mode='rgb_array').transpose((2, 0, 1))
+    _, screen_height, screen_width = screen.shape
+    screen = screen[:, int(screen_height * 0.4):int(screen_height * 0.8)]
+    view_width = int(screen_width * 0.6)
+    cart_location = get_cart_location(env, screen_width)
+    if cart_location < view_width // 2:
+        slice_range = slice(view_width)
+    elif cart_location > (screen_width - view_width // 2):
+        slice_range = slice(-view_width, None)
+    else:
+        slice_range = slice(cart_location - view_width // 2,
+                            cart_location + view_width // 2)
+
+    screen = screen[:, :, slice_range]
+    screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+    screen = torch.from_numpy(screen)
+
+    return resize(screen).unsqueeze(0).to(device)
+
+
+def select_action(state, n_actions, policy_net, device, eps_start, eps_end, decay, steps_done):
+    sample = random.random()
+    eps_threshold = eps_end + (eps_start - eps_end) * \
+                    np.exp(-1. * steps_done / decay)
+    steps_done += 1
+    if sample > eps_threshold:
+        with torch.no_grad():
+            return policy_net(state).max(1)[1].view(1, 1)
+    else:
+        return torch.tensor([[random.randrange(n_actions)]],
+                            device=device, dtype=torch.long)
+
+
+def plot_durations(episode_durations):
+    plt.figure(2)
+    durations_t = torch.tensor(episode_durations, dtype=torch.float)
+    plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(durations_t.numpy())
+
+    if len(durations_t) >= 100:
+        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+        means = torch.cat((torch.zeros(99), means))
+        plt.plot(means.numpy())
+
+    display.clear_output(wait=True)
+    plt.show()
+
+
+def optimize_model(memory, batch_size, device, policy_net, target_net, gamma, optimizer):
+    if len(memory) < batch_size:
+        return
+    transitions = memory.sample(batch_size)
+    batch = Transition(*zip(*transitions))
+
+    non_final_mask = torch.tensor(
+        tuple(map(lambda s: s is not None, batch.next_state)),
+        device=device, dtype=torch.bool)
+    non_final_next_states = torch.cat(
+        [s for s in batch.next_state if s is not None])
+
+    state_batch = torch.cat(batch.state)
+    action_batch = torch.cat(batch.action)
+    reward_batch = torch.cat(batch.reward)
+
+    state_action_values = policy_net(state_batch).gather(1, action_batch)
+
+    next_state_values = torch.zeros(batch_size, device=device)
+    next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
+    expected_state_action_values = (next_state_values * gamma) + reward_batch
+
+    loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
+
+    optimizer.zero_grad()
+    loss.backward()
+    for param in policy_net.parameters():
+        param.grad.data.clamp_(-1, 1)
+    optimizer.step()
+
+    return target_net, policy_net
